8f64419e99141787871b56097644d3da7c2f59f9
mtcraft22 <matort222@gmail.com>
2022-11-23 13:57:11 +0100

    arenglan notis

--- main.py
+++ main.py
@@ -1,41 +1,58 @@
 import tkinter,noti
 
-root=tkinter.Tk()
-root.geometry("800x800")
 
-hasnotified=tkinter.IntVar()
+class todo_main:
+    def __init__(self):
+        self.root=tkinter.Tk()
+        self.root.geometry("800x800")
 
-listanotis=tkinter.Canvas(root,bg="white").grid(row=0,column=0,rowspan=100)
-tittle_label=tkinter.Label(root,text="  Nombre: ").grid(row=0,column=1,padx=2)
-tittle_entry=tkinter.Entry(root,width=32)
-tittle_entry.grid(row=0,column=2,padx=2)
-desc_label=tkinter.Label(root,text="Descrición: ").grid(row=1,column=1,padx=2)
-desc_entry=tkinter.Text(root,height=5,width=32)
-desc_entry.grid(row=1,column=2,padx=2,rowspan=4)
+        self.hasnotified=tkinter.IntVar()
+        self.priority_level=tkinter.IntVar()
 
-Date_label=tkinter.Label(root,text="Fecha: ").grid(row=7,column=1,padx=2)
-Date_entry=tkinter.Entry(root,width=32,state='disabled')
-Date_entry.grid(row=7,column=2,padx=2)
-def enable(hasnotifiedp):
-    print(hasnotifiedp)
-    adv_label=tkinter.Label(root,text="  ")
-    adv_label.grid(row=5,column=1,padx=2)
-    if hasnotifiedp.get()==0:
-        Date_entry.config(state='disabled')
-        desc_label
-    else:
-        Date_entry.config(state='normal')
-def get_all():
-    tittle=tittle_entry.get()
-    desc=desc_entry.get(1.0,tkinter.END)
-    date=Date_entry.get()
-    print(tittle)
-    if tittle:
-        a=noti.noti(title=tittle,date=date,description=desc,priority=None)
-        a.make_label(listanotis)
+        self.listanotis=tkinter.Canvas(self.root,bg="white").grid(row=0,column=0,rowspan=100)
 
-   
+        self.tittle_label=tkinter.Label(self.root,text="  Nombre: ").grid(row=0,column=1,padx=2)
 
-noticheck=tkinter.Checkbutton(root,text="Quiero recibir una notificación de la tarea (WIP)",variable=hasnotified,command=lambda:enable(hasnotified)).grid(row=6,column=2,padx=2)
-Submit=tkinter.Button(root,text="Añadir tarea",command=get_all).grid(row=5,column=2,padx=2)
-root.mainloop()
+        self.tittle_entry=tkinter.Entry(self.root,width=32)
+        self.tittle_entry.grid(row=0,column=2,padx=2,columnspan=4)
+
+        self.desc_label=tkinter.Label(self.root,text="Descrición: ").grid(row=1,column=1,padx=2)
+
+        self.desc_entry=tkinter.Text(self.root,height=5,width=32)
+        self.desc_entry.grid(row=1,column=2,padx=2,rowspan=4,columnspan=4)
+
+        self.Date_label=tkinter.Label(self.root,text="Fecha: ").grid(row=7,column=1,padx=2)
+
+        self.Date_entry=tkinter.Entry(self.root,width=32,state='disabled')
+        self.Date_entry.grid(row=7,column=2,padx=2,columnspan=4)
+
+        self.noticheck=tkinter.Checkbutton(self.root,text="Quiero recibir una notificación de la tarea (WIP)",variable=self.hasnotified,command=lambda:self.enable(self.hasnotified)).grid(row=6,column=2,padx=2,columnspan=4)
+        
+        self.Submit=tkinter.Button(self.root,text="Añadir tarea",command=self.get_all).grid(row=5,column=2,padx=2,columnspan=4)
+        
+        self.priority_label=tkinter.Label(self.root,text="Priodidad: ").grid(row=8,column=1,padx=2)
+
+        self.priority_level_0=tkinter.Radiobutton(self.root,text="Baja",variable=self.priority_level,value=0).grid(row=8,column=2,padx=2) 
+        self.priority_level_0=tkinter.Radiobutton(self.root,text="Media",variable=self.priority_level,value=1).grid(row=8,column=3,padx=2)
+        self.priority_level_0=tkinter.Radiobutton(self.root,text="Alta",variable=self.priority_level,value=2).grid(row=8,column=4,padx=2)
+        self.priority_level_0=tkinter.Radiobutton(self.root,text="Urgente",variable=self.priority_level,value=3).grid(row=8,column=5,padx=2)
+
+        self.root.mainloop()
+    def enable(self,hasnotifiedp):
+        adv_label=tkinter.Label(self.root,text="  ")
+        adv_label.grid(row=5,column=1,padx=2)
+        if hasnotifiedp.get()==0:
+            self.Date_entry.config(state='disabled')
+        else:
+            self.Date_entry.config(state='normal')
+    def get_all(self):
+        tittle=self.tittle_entry.get()
+        desc=self.desc_entry.get(1.0,tkinter.END)
+        date=self.Date_entry.get()
+        if tittle:
+            a=noti.noti(title=tittle,date=date,description=desc,priority=None)
+            a.make_label(self.listanotis)
+
+
+app=todo_main()
+
--- noti.py
+++ noti.py
@@ -1,17 +1,28 @@
 import tkinter
 
 notis=[]
+ocupiced_row=[]
 class noti:
+   
     def __init__(self,**kwargs):
         self.date=kwargs["date"]
         self.description=kwargs["description"]
         self.tittle=kwargs["title"]
         self.priority=kwargs["priority"]
+        self.indexocupated=0
         notis.append(self)
         
     def make_label(self,canvas):
+        print("lon ",len(notis))
+        ocupiced_row.append(len(notis))
+        print(ocupiced_row)
         self.label=tkinter.Label(canvas,bg="white",text=self.tittle)
-        self.label.grid(row=len(notis)-1,column=0)
+        if not(ocupiced_row[-1]+1 in ocupiced_row):
+            self.label.grid(row=ocupiced_row[-1]+1,column=0)
+            self.indexocupated==ocupiced_row[-1]
+        else:
+            self.label.grid(row=len(notis)+1,column=0)
+            self.indexocupated==len(notis)
         self.label.bind('<Button-1>', self.show_info)
     def show_info(self, event):
         self.info=tkinter.Toplevel()
@@ -24,6 +35,7 @@ class noti:
         self.info.destroy()
         self.label.destroy()
         notis.remove(self)
-        print(notis)
+        ocupiced_row.pop(self.indexocupated)
+        print(ocupiced_row)
         del(self)
 
