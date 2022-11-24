3ef7bc2f2cc1ed678ece6aea62f8ca51262200df
mtcraft22 <matort222@gmail.com>
2022-11-24 12:34:12 +0100

    holiwi

--- main.py
+++ main.py
@@ -1,58 +1,78 @@
-import tkinter,noti
+import tkinter, noti
 
 
 class todo_main:
     def __init__(self):
-        self.root=tkinter.Tk()
+        self.root = tkinter.Tk()
         self.root.geometry("800x800")
 
-        self.hasnotified=tkinter.IntVar()
-        self.priority_level=tkinter.IntVar()
+        self.hasnotified = tkinter.IntVar()
+        self.priority_level = tkinter.StringVar()
 
-        self.listanotis=tkinter.Canvas(self.root,bg="white").grid(row=0,column=0,rowspan=100)
+        self.listanotis = tkinter.Canvas(self.root, bg="white", width=200, height=10000).grid(row=0, column=0, rowspan=1000, padx=5,
+                                                                            pady=5)
 
-        self.tittle_label=tkinter.Label(self.root,text="  Nombre: ").grid(row=0,column=1,padx=2)
+        self.tittle_label = tkinter.Label(self.root, text="  Nombre: ")
+        self.tittle_label.grid(row=0, column=1, padx=2)
 
-        self.tittle_entry=tkinter.Entry(self.root,width=32)
-        self.tittle_entry.grid(row=0,column=2,padx=2,columnspan=4)
+        self.tittle_entry = tkinter.Entry(self.root, width=32)
+        self.tittle_entry.grid(row=0, column=2, padx=2, columnspan=4)
 
-        self.desc_label=tkinter.Label(self.root,text="Descrición: ").grid(row=1,column=1,padx=2)
+        self.desc_label = tkinter.Label(self.root, text="Descrición: ")
+        self.desc_label.grid(row=1, column=1, padx=2)
 
-        self.desc_entry=tkinter.Text(self.root,height=5,width=32)
-        self.desc_entry.grid(row=1,column=2,padx=2,rowspan=4,columnspan=4)
+        self.desc_entry = tkinter.Text(self.root, height=5, width=32)
+        self.desc_entry.grid(row=1, column=2, padx=2, rowspan=4, columnspan=4)
 
-        self.Date_label=tkinter.Label(self.root,text="Fecha: ").grid(row=7,column=1,padx=2)
+        self.Date_label = tkinter.Label(self.root, text="Fecha: ")
+        self.Date_label.grid(row=7, column=1, padx=2)
 
-        self.Date_entry=tkinter.Entry(self.root,width=32,state='disabled')
-        self.Date_entry.grid(row=7,column=2,padx=2,columnspan=4)
+        self.Date_entry = tkinter.Entry(self.root, width=32, state='disabled')
+        self.Date_entry.grid(row=7, column=2, padx=2, columnspan=4)
 
-        self.noticheck=tkinter.Checkbutton(self.root,text="Quiero recibir una notificación de la tarea (WIP)",variable=self.hasnotified,command=lambda:self.enable(self.hasnotified)).grid(row=6,column=2,padx=2,columnspan=4)
-        
-        self.Submit=tkinter.Button(self.root,text="Añadir tarea",command=self.get_all).grid(row=5,column=2,padx=2,columnspan=4)
-        
-        self.priority_label=tkinter.Label(self.root,text="Priodidad: ").grid(row=8,column=1,padx=2)
+        self.noticheck = tkinter.Checkbutton(self.root, text="Quiero recibir una notificación de la tarea (WIP)",
+                                             variable=self.hasnotified, command=lambda: self.enable(self.hasnotified))
+        self.noticheck.grid(row=6, column=2, padx=2, columnspan=4)
 
-        self.priority_level_0=tkinter.Radiobutton(self.root,text="Baja",variable=self.priority_level,value=0).grid(row=8,column=2,padx=2) 
-        self.priority_level_0=tkinter.Radiobutton(self.root,text="Media",variable=self.priority_level,value=1).grid(row=8,column=3,padx=2)
-        self.priority_level_0=tkinter.Radiobutton(self.root,text="Alta",variable=self.priority_level,value=2).grid(row=8,column=4,padx=2)
-        self.priority_level_0=tkinter.Radiobutton(self.root,text="Urgente",variable=self.priority_level,value=3).grid(row=8,column=5,padx=2)
+        self.Submit = tkinter.Button(self.root, text="Añadir tarea", command=self.get_all)
+        self.Submit.grid(row=5, column=2, padx=2, columnspan=4)
+
+        self.priority_label = tkinter.Label(self.root, text="Priodidad: ")
+        self.priority_label.grid(row=8, column=1, padx=2)
+
+        self.priority_level_0 = tkinter.Radiobutton(self.root, text="Baja", variable=self.priority_level, value="Baja",
+                                                    tristatevalue="x")
+        self.priority_level_0.grid(row=8, column=2, padx=2)
+
+        self.priority_level_1 = tkinter.Radiobutton(self.root, text="Media", variable=self.priority_level, value="Media"
+                                                    , tristatevalue="x")
+        self.priority_level_1.grid(row=8, column=3, padx=2)
+
+        self.priority_level_2 = tkinter.Radiobutton(self.root, text="Alta", variable=self.priority_level, value="Alta"
+                                                    , tristatevalue="x")
+        self.priority_level_2.grid(row=8, column=4, padx=2)
+
+        self.priority_level_3 = tkinter.Radiobutton(self.root, text="Urgente", variable=self.priority_level,
+                                                    value="Urgente", tristatevalue="x")
+        self.priority_level_3.grid(row=8, column=5, padx=2)
 
         self.root.mainloop()
-    def enable(self,hasnotifiedp):
-        adv_label=tkinter.Label(self.root,text="  ")
-        adv_label.grid(row=5,column=1,padx=2)
-        if hasnotifiedp.get()==0:
+
+    def enable(self, hasnotifiedp):
+        adv_label = tkinter.Label(self.root, text="  ")
+        adv_label.grid(row=5, column=1, padx=2)
+        if hasnotifiedp.get() == 0:
             self.Date_entry.config(state='disabled')
         else:
             self.Date_entry.config(state='normal')
+
     def get_all(self):
-        tittle=self.tittle_entry.get()
-        desc=self.desc_entry.get(1.0,tkinter.END)
-        date=self.Date_entry.get()
+        tittle = self.tittle_entry.get()
+        desc = self.desc_entry.get(1.0, tkinter.END)
+        date = self.Date_entry.get()
         if tittle:
-            a=noti.noti(title=tittle,date=date,description=desc,priority=None)
+            a = noti.noti(title=tittle, date=date, description=desc, priority=self.priority_level.get())
             a.make_label(self.listanotis)
 
 
-app=todo_main()
-
+app = todo_main()
--- noti.py
+++ noti.py
@@ -1,28 +1,41 @@
 import tkinter
-notis=[]
+
+notis = []
+
+
 class noti:
-    def __init__(self,**kwargs):
-        self.date=kwargs["date"]
-        self.description=kwargs["description"]
-        self.tittle=kwargs["title"]
-        self.priority=kwargs["priority"]
-        self.label=None
+    def __init__(self, **kwargs):
+        self.date = kwargs["date"]
+        self.description = kwargs["description"]
+        self.tittle = kwargs["title"]
+        self.priority = kwargs["priority"]
+        self.label = None
+        self.info = None
         notis.append(self)
-    def make_label(self,canvas):  
-        print(notis)
-        self.label=tkinter.Label(canvas,bg="white",text=self.tittle)
-        self.label.grid(row=notis.index(notis[-1])+1,column=0)
-        self.label.bind("<Button-1>",self.show_info)
+
+    def make_label(self, canvas):
+        self.label = tkinter.Label(canvas, bg="white", text=self.tittle)
+        self.label.grid(row=notis.index(notis[-1]) + 1, column=0)
+        self.label.bind("<Button-1>", self.show_info)
+
     def show_info(self, event):
-        self.info=tkinter.Toplevel()
-        label0=tkinter.Label(self.info,text="Descrición: ").grid(row=1,column=0)
-        label1=tkinter.Label(self.info,text=self.description).grid(row=1,column=1)
-        label2=tkinter.Label(self.info,text="Fecha notificación: ").grid(row=2,column=0)
-        label3=tkinter.Label(self.info,text=self.date).grid(row=2,column=1)
-        buttondel=tkinter.Button(self.info,text="Eliminar tarea",command=self.removenoti).grid(row=3,column=0)
+        self.info = tkinter.Toplevel()
+        label0 = tkinter.Label(self.info, text="Descrición: ")
+        label0.grid(row=1, column=0)
+        label1 = tkinter.Label(self.info, text=self.description)
+        label1.grid(row=1, column=1)
+        label2 = tkinter.Label(self.info, text="Fecha notificación: ")
+        label2.grid(row=2, column=0)
+        label3 = tkinter.Label(self.info, text=self.date)
+        label3.grid(row=2, column=1)
+        label4 = tkinter.Label(self.info, text="Priodidad: ")
+        label4.grid(row=3, column=0)
+        label5 = tkinter.Label(self.info, text=self.priority)
+        label5.grid(row=3, column=1)
+        buttondel = tkinter.Button(self.info, text="Eliminar tarea", command=self.removenoti)
+        buttondel.grid(row=4, column=0)
+
     def removenoti(self):
         self.info.destroy()
         self.label.destroy()
-        notis.remove(self)
-        del(self)
-
+        del self
