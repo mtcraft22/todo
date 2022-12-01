b4bd5dc6dc3137510cd92c2e6f5127b820e65a8e
Marc Tort Pascual <marctorpas@campus.monlau.com>
2022-12-01 15:17:16 +0100

    Added gitignore policy  Committer: Marc Tort Pascual <marctorpas@campus.monlau.com>
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	new file:   .gitignore
    	modified:   checknotifications.py
    	modified:   stringtodateintsds.py

--- stringtodateintsds.py
+++ stringtodateintsds.py
@@ -1,10 +1,1 @@
-def str2int(sateinput):
-    firtsdiv=sateinput.split(", ")
-    day=firtsdiv[0].split("/")
-    time=firtsdiv[1].split(":")
-    day.extend(time)
-    return day
 
-
-
-print(str2int("22/11/24, 9:1:0"))
